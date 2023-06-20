27be846e0c5ca1f76b64293d8c198dc0d78d9219
cengizhankkaya <cengizhan.kaya.1453@gmail.com>
2023-06-20 16:19:38 +0300

    Update urls.py

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.urls import path
-from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from blogapp import views
 
 
 urlpatterns = [
